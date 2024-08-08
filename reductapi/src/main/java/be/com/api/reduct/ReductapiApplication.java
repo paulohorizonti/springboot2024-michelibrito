d9bfd469ea380ffd4ff8f7589ed154e804118eb7
tupaerp <tupaerp@gmail.com>
2024-08-08 11:58:57 -0300

    novo commit

---
+++ reductapi/src/main/java/be/com/api/reduct/ReductapiApplication.java
@@ -0,0 +1,13 @@
+package be.com.api.reduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReductapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReductapiApplication.class, args);
+	}
+
+}
---
+++ reductapi/src/test/java/be/com/api/reduct/ReductapiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.com.api.reduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReductapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
